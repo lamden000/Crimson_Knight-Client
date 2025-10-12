6582a9c238d03ee09e8516ae2423fb5ed3026cac
letan <zasure0803@gmail.com>
2025-10-13 03:34:48 +0700

    UI Dialog
    Thêm các dialog YesNo, OK, DropDown

---
+++ Assets/Scripts/UI/DialogDropdown.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class DialogDropdown : DialogBase
+{
+    [Header("References")]
+    [SerializeField] private TextMeshProUGUI titleText;
+    [SerializeField] private Transform contentParent;  
+    [SerializeField] private Button itemPrefab;        
+    [SerializeField] private Button closeButton;      
+    private Action<int> onSelected;
+
+    public void Setup(string title, string[] options, Action<int> callback)
+    {
+        titleText.text = title;
+        onSelected = callback;
+
+        // Xóa nút cũ
+        foreach (Transform child in contentParent)
+            Destroy(child.gameObject);
+
+        // Tạo nút mới
+        for (int i = 0; i < options.Length; i++)
+        {
+            int index = i;
+            Button item = Instantiate(itemPrefab, contentParent);
+
+            var label = item.GetComponentInChildren<TextMeshProUGUI>();
+            if (label != null)
+                label.text = options[i];
+
+            item.onClick.RemoveAllListeners();
+            item.onClick.AddListener(() =>
+            {
+                onSelected?.Invoke(index);
+                Close();
+            });
+        }
+        closeButton.onClick.RemoveAllListeners();
+        closeButton.onClick.AddListener(Close);
+        Canvas.ForceUpdateCanvases();
+        var rect = contentParent as RectTransform;
+        LayoutRebuilder.ForceRebuildLayoutImmediate(rect);
+    }
+
+}
---
+++ Assets/Scripts/UI/UITest.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class UITest : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("[UITest] Start() called. UIManager.Instance = " + (UIManager.Instance != null));
+        // Test hộp thoại Yes/No
+        UIManager.Instance.ShowYesNo("Bạn có chắc muốn xóa vật phẩm?", 5, (yes, id) =>
+        {
+            if (yes)
+                Debug.Log($"Yes được nhấn ở dialog {id}");
+            else
+                Debug.Log($"No được nhấn ở dialog {id}");
+        });
+
+
+        // Test Dropdown
+        //string[] options = { "Kiếm", "Khiên", "Cung", "Gậy phép" };
+
+        //UIManager.Instance.ShowDropdown("Chọn vũ khí:", options, index =>
+        //{
+        //    Debug.Log($"Bạn đã chọn: {index} - {options[index]}");
+        //});
+
+        // Test OK Dialog
+
+        //UIManager.Instance.ShowOK("Chúc mừng bạn đã chiến thắng!", () =>
+        //{
+        //    Debug.Log("Đã nhấn OK");
+        //});
+
+    }
+}
