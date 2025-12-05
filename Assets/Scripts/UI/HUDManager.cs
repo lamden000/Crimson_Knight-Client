c948806718faeacb07f5e70046bf577df3859bea
letan <zasure0803@gmail.com>
2025-12-05 15:51:28 +0700

    UI Skill Panel
    Add UI Skill Panel

--- Assets/Scripts/UI/HUDManager.cs
+++ Assets/Scripts/UI/HUDManager.cs
@@ -9,10 +9,19 @@ public class HUDManager : MonoBehaviour
     private float maxHPWidth;
     private float maxMPWidth;
 
+    [Header("Skills")]
+    public SkillSlot[] skillSlots = new SkillSlot[8];
+
     void Start()
     {
         maxHPWidth = hpBar.sizeDelta.x;
         maxMPWidth = mpBar.sizeDelta.x;
+
+        for (int i = 0; i < skillSlots.Length; i++)
+        {
+            skillSlots[i].skillIndex = i + 1;
+            skillSlots[i].Init();
+        }
     }
 
     void Update()
@@ -26,5 +35,19 @@ public class HUDManager : MonoBehaviour
         // Cập nhật MP
         float mpRatio = player.CurrentMp / player.MaxMp;
         mpBar.sizeDelta = new Vector2(maxMPWidth * mpRatio, mpBar.sizeDelta.y);
+
+        UpdateSkillInputs();
+    }
+
+    void UpdateSkillInputs()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) skillSlots[0].TryUseSkill();
+        if (Input.GetKeyDown(KeyCode.Alpha2)) skillSlots[1].TryUseSkill();
+        if (Input.GetKeyDown(KeyCode.Alpha3)) skillSlots[2].TryUseSkill();
+        if (Input.GetKeyDown(KeyCode.Alpha4)) skillSlots[3].TryUseSkill();
+        if (Input.GetKeyDown(KeyCode.Alpha5)) skillSlots[4].TryUseSkill();
+        if (Input.GetKeyDown(KeyCode.Alpha6)) skillSlots[5].TryUseSkill();
+        if (Input.GetKeyDown(KeyCode.Alpha7)) skillSlots[6].TryUseSkill();
+        if (Input.GetKeyDown(KeyCode.Alpha8)) skillSlots[7].TryUseSkill();
     }
 }
---
+++ Assets/Scripts/UI/SkillSlot.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class SkillSlot : MonoBehaviour
+{
+    public int skillIndex;                
+    public string skillName = "Skill";
+    public float cooldownTime = 3f;
+
+    public Image icon;
+    public Image cooldownOverlay;
+
+    float cooldownRemaining = 0;
+
+    public void Init()
+    {
+        Sprite s = Resources.Load<Sprite>($"UI/Skills/skill{skillIndex}");
+        if (s != null) icon.sprite = s;
+
+        cooldownOverlay.fillAmount = 0;
+    }
+
+    void Update()
+    {
+        if (cooldownRemaining > 0)
+        {
+            cooldownRemaining -= Time.deltaTime;
+            cooldownOverlay.fillAmount = cooldownRemaining / cooldownTime;
+        }
+        KeyCode key = KeyCode.Alpha1 + (skillIndex - 1);
+        if (Input.GetKeyDown(key))
+        {
+            TryUseSkill();
+        }
+    }
+
+    public void TryUseSkill()
+    {
+        if (cooldownRemaining > 0)
+        {
+            Debug.Log($"{skillName}{skillIndex} is on cooldown ({cooldownRemaining:F1}s)");
+            return;
+        }
+
+        Debug.Log($"Using skill: {skillName}{skillIndex}");
+
+        cooldownRemaining = cooldownTime;
+        cooldownOverlay.fillAmount = 1;
+    }
+}
