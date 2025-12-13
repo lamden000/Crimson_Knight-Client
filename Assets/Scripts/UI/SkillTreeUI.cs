dc358ac3ce3e4991dc1c24b183abc5af46f83422
letan <zasure0803@gmail.com>
2025-12-14 01:38:35 +0700

    FIX UI

--- Assets/Scripts/UI/SkillTreeUI.cs
+++ Assets/Scripts/UI/SkillTreeUI.cs
@@ -1,30 +1,38 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+
 public class SkillTreeUI : MonoBehaviour
 {
-    public SkillNode[] skillNodes;   
+    public SkillNode[] skillNodes;
     public SkillInfoPanel infoPanel;
     public Button closeButton;
-    SkillData[] skillList;
     public GameObject menuRoot;
+
+    SkillData[] skillList;
+
     void Start()
     {
-        LoadSkillList();     // tạo dữ liệu
+        Debug.Log("SkillTreeUI START");
+
+        LoadSkillList();
         LoadSkillNodes();
-        // đưa dữ liệu vào node
+
+        closeButton.onClick.RemoveAllListeners();
         closeButton.onClick.AddListener(CloseAllMenu);
     }
+
     void CloseAllMenu()
     {
-        menuRoot.SetActive(false);      // ← TẮT TOÀN BỘ MENU
+        menuRoot.SetActive(false);
     }
+
     void LoadSkillList()
     {
         skillList = new SkillData[]
         {
             new SkillData(0,"Gốc","Skill gốc",10,2,30,0),
-            new SkillData(1,"Nhanh 1","Mô tả",10,1,10,1),
-            new SkillData(2,"Nhanh 2","Mô tả",10,1,10,2),
+            new SkillData(1,"Nhánh 1","Mô tả",10,1,10,1),
+            new SkillData(2,"Nhánh 2","Mô tả",10,1,10,2),
             new SkillData(3,"Nhánh 1.1","Mô tả",10,1,10,3),
             new SkillData(4,"Nhánh 1.2","Mô tả",10,1,10,4),
             new SkillData(5,"Nhánh 2.1","Mô tả",10,1,10,5),
@@ -34,18 +42,13 @@ public class SkillTreeUI : MonoBehaviour
 
     void LoadSkillNodes()
     {
-        for (int i = 0; i < skillNodes.Length; i++)
+        int count = Mathf.Min(skillNodes.Length, skillList.Length);
+
+        Debug.Log($"LoadSkillNodes count = {count}");
+
+        for (int i = 0; i < count; i++)
         {
-            skillNodes[i].Load(skillList[i]);
-
-            int id = i;  
-
-            skillNodes[i].GetComponent<UnityEngine.UI.Button>().onClick.AddListener(() =>
-            {
-                Debug.Log("Bạn đã click vào skill ID = " + skillList[id].id);
-                infoPanel.ShowSkill(skillList[id]);
-            });
+            skillNodes[i].Load(skillList[i], infoPanel);
         }
     }
-
 }
