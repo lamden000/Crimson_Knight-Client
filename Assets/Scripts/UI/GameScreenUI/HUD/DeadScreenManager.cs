4da7682fe1ff43e240101617f05049beb24b3486
letan <zasure0803@gmail.com>
2026-01-21 18:33:09 +0700

    Small Change

---
+++ Assets/Scripts/UI/GameScreenUI/HUD/DeadScreenManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DeadScreenManager : MonoBehaviour
+{
+    public Button btnRespawn;
+    public Button btnRecall;
+
+    private void OnEnable()
+    {
+        Debug.Log("DeadScreen Enabled");
+
+        btnRespawn.onClick.RemoveAllListeners();
+        btnRecall.onClick.RemoveAllListeners();
+
+        btnRespawn.onClick.AddListener(OnClickRespawn);
+        btnRecall.onClick.AddListener(OnClickRecall);
+    }
+
+    private void OnClickRespawn()
+    {
+        Debug.Log("Đã nhấn: Respawn");
+    }
+
+    private void OnClickRecall()
+    {
+        Debug.Log("Đã nhấn: Recall");
+    }
+}
